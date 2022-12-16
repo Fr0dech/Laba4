3a46d761418f9d3cc62bfa4c4d219309f39dc08e
Fr0dech <mikhstas@mail.ru>
2022-12-16 23:45:30 +0200

    added 4.1 & 4.3

--- Illhavefivenumbernines/Illhavefivenumbernines.cpp
+++ Illhavefivenumbernines/Illhavefivenumbernines.cpp
@@ -4,6 +4,18 @@
 
 
 
+bool isPrime(int a) {
+    bool flag = true;
+    if (a < 2) {
+        return false;
+    }
+    for (int i = 2; i < std::sqrt(a); i++) {
+        if (a % i == 0) {
+            flag = false;
+        }
+    }
+    return flag;
+}
 void insertionSort(int list[], int listLength)
 {
     for (int i = 1; i < listLength; i++)
@@ -44,45 +56,29 @@ int main()
     {
     case 1:
     {
-        bool isPrime = false;
-        int n, chislo;
+        int l;
+        int array[10000];
         std::cout << "Enter the amount of numbers in your sequence: ";
-        std::cin >> n;
-        int mas[10000];
-        for (int i = 0; i < n; i++)
-        {
-            std::cin >> chislo;
-            for (int chi = 2; chi <= sqrt(chislo); chi++)
-                if (chislo % chi == 0)
-                {
-                    isPrime = true;
-                    break;
-                }
-                else
-                {
-
-                    break;
-                }
-            mas[i] = chislo;
-        }
-        if (isPrime)
-        {
-            insertionSort(mas, n);
-
-            for (int i = 0; i < n; i++)
-            {
-                std::cout << mas[i] << " ";
+        std::cin >> l;
+        std::cout << "Enter numbers." << std::endl;
+        bool flag = false;
+        for (int i = 0; i < l; i++) {
+            std::cin >> array[i];
+            if (isPrime(array[i])) {
+                flag = true;
             }
         }
-        else
-        {
-            for (int i = 0; i < n; i++)
-            {
-                std::cout << mas[i] << " ";
-            }
+        if (flag == true) {
+            insertionSort(array, l);
         }
+        for (int i = 0; i < l; i++) {
+            std::cout << array[i] << " ";
+        }
+        break;
     }
+
     case 2:
+    {
         int n, chislo, proizv = 1;
         std::cout << "Enter the amount of numbers in your sequence: ";
         std::cin >> n;
@@ -93,11 +89,11 @@ int main()
         {
             std::cin >> chislo;
             mas[i] = chislo;
-                for (proizv = 1; chislo; chislo /= 10)
-                    proizv *= chislo % 10;
+            for (proizv = 1; chislo; chislo /= 10)
+                proizv *= chislo % 10;
             masmult[i] = proizv;
         }
-        
+
         for (int i = 0; i < n; i++)
         {
             int s = mas[i];
@@ -105,7 +101,7 @@ int main()
                 s /= 10;
             evenmult[i] = s;
         }
-        insertionSort1(evenmult,mas, masmult, n);
+        insertionSort1(evenmult, mas, masmult, n);
         insertionSort1(masmult, mas, evenmult, n);
         for (int i = 0; i < n - 1; i++)
         {
@@ -119,7 +115,7 @@ int main()
                     evenmult[i] = evenmult[i + 1];
                     evenmult[i + 1] = c;
                 }
-                else if (evenmult[i] == evenmult[i+1])
+                else if (evenmult[i] == evenmult[i + 1])
                     if (mas[i] > mas[i + 1])
                     {
                         int c = mas[i];
@@ -127,11 +123,50 @@ int main()
                         mas[i + 1] = c;
                     }
         }
-        
+
 
 
         for (int i = 0; i < n; i++)
             std::cout << mas[i] << " ";
+        break;
+    }
+    case 3:
+    {
+        int a[100][100];
+        int n, m;
+        std::cout << "Enter amount of rows and columns." << std::endl;
+        std::cin >> n >> m;
+        int max = 100000000;
+        int max_ind = -1;
+        std::cout << "Enter numbers." << std::endl;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < m; j++) {
+                std::cin >> a[i][j];
+                if (a[i][j] % 2 != 0) {
+                    count++;
+                }
+            }
+            if (count > max) {
+                max = count;
+                max_ind = i;
+            }
+        }
+        std::cout << '\n';
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == max_ind && max_ind != -1) {
+                    std::cout << a[i][j] * a[i][j] << " ";
+                }
+                else
+                {
+                    std::cout << a[i][j] << " ";
+                }
+            }
+            std::cout << '\n';
+        }
+        break;
+    }
     }
 }
 
